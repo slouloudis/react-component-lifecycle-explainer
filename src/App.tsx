e9208414d368f3bd93d7675bc022541d21b368df
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-06-06 16:05:58 +0100

    init tailwind ect

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { ElemFuncComp } from './components/animationComps/ElemFuncComp'
+function App() {
+  return (
+    <>
+    <p>Use the arrow keys to navigate</p>
+    <ElemFuncComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/animationComps/ElemFuncComp.tsx
@@ -0,0 +1,9 @@
+import {motion} from "framer-motion"
+
+export function ElemFuncComp() {
+    return (
+        <motion.div>
+
+        </motion.div>
+    )
+}
